e85897ecc2bb340ab8b4e8d94cd31f72582972b0
Waylon S. Walker <waylon@waylonwalker.com>
2023-01-21 16:32:20 -0600

    implement load_prompt

--- lockhart/prompts.py
+++ lockhart/prompts.py
@@ -1,10 +1,8 @@
-import ast
-from collections import namedtuple
+import copy
 import os
 import subprocess
 import sys
 import tempfile
-import textwrap
 from typing import Optional
 
 from jinja2 import Template
@@ -12,64 +10,28 @@ import openai
 
 from lockhart.config import config
 
-# Python 3.10
 
-
-def load_prompt(prompt: str, config: dict) -> dict:
+def load_prompt(prompt: str) -> dict:
     """
-    loads the configuration for the prompt from config.  Finds the
+    Loads the configuration for the prompt from config.  Finds the
     corresponding profile and unpacks the prompt config into that profile.
     """
-    profile_name = config["prompts"]["profile"]
-    profile = config["profiles"]
-    prompt_config = config["prompts"]
+    profile_name = config["prompts"][prompt]["profile"]
+    profile = copy.deepcopy(config["profiles"][profile_name])
+    prompt_config = config["prompts"][prompt]
     for key, value in prompt_config.items():
         if key != "profile":
-            profile = value
+            profile.update({key: value})
     return profile
-
-
-# here is an example config
-
-{
-    "profiles": {
-        "code": {
-            "engine": "text-davinci-003",
-            "temperature": 0,
-            "max_tokens": 150,
-            "top_p": 1.0,
-            "frequency_penalty": 0.0,
-            "presence_penalty": 0.0,
-        }
-    },
-    "prompts": {
-        "docstring": {
-            "profile": "code",
-            "stop": ["#", '"""'],
-            "prompt": "# Python 3.10\n\n{{code}}\n\n# Please write a high quality python",
-        },
-        "complete-func": {
-            "engine": "text-davinci-003",
-            "temperature": 0,
-            "max_tokens": 500,
-            "top_p": 1.0,
-            "frequency_penalty": 0.0,
-            "presence_penalty": 0.0,
-            "stop": ["xxx", "???"],
-            "prompt": "# python3.10\n\nfill in the following python function to do what the docstring says to do\n\n{{ text }}\n---\n",
-        },
-    },
-}
 
 
 def run_configured_prompt(prompt: str, dry_run: bool, edit: bool) -> Optional[str]:
 
-    # parsed = parse_function(code)
     text = ""
     for line in sys.stdin:
         text = text + line
 
-    prompt = config.get("prompts", {}).get(prompt)
+    prompt = load_prompt(prompt)
     template = Template(prompt["prompt"])
     prompt["prompt"] = template.render(input=input, text=text)
     if edit:
@@ -82,8 +44,6 @@ def run_configured_prompt(prompt: str, dry_run: bool, edit: bool) -> Optional[st
         prompt["prompt"] = file.read().decode()
         if res != 0:
             return "editor quit"
-        else:
-            return "editor not quit"
 
     if prompt is None:
         raise KeyError(f"{prompt} is not configured")
@@ -94,7 +54,6 @@ def run_configured_prompt(prompt: str, dry_run: bool, edit: bool) -> Optional[st
     response = openai.Completion.create(**prompt)
     text = response["choices"][0]["text"]
     return response
-    return text
 
 
 def list_args(func: callable):
@@ -102,162 +61,3 @@ def list_args(func: callable):
     args = func.__code__.co_varnames
     return args
     return list(func.__code__.co_varnames)
-
-
-def write_docstring(code):
-
-    parsed = parse_function(code)
-    prompt = f'''
-# Python 3.10
-
-{code}
-
-# Please write a high quality python docstring conforming to the google code style for docstrings for the above code.
-
-The name of the function is: ```{parsed.name}```.
-
-It has the following signature {parsed.args}.
-
-Do not return the full function.
-Only return the docstring.
-Include an example if you can.
-It should start with a short summary written in an imperative mood.
-followed by a newline.
-followed by a short description.
-followed by another newline.
-then followed by any of the following sections sections if they apply to this function (Args: , Returns: , Raises: , Yields: , Note: , Example: )
-"""
-'''
-    # prompt = f"Please write a python docstring conforming to the google code style for docstrings.\n\n{code}\n"
-    # prompt = f'# Python 3.10\n{code}\n\n# An elaborate, high quality docstring for the above function:\n"""'
-
-    print(f"generating a response for \n\n{prompt}")
-    print("-" * 80)
-    print()
-
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        temperature=0,
-        max_tokens=150,
-        top_p=1.0,
-        frequency_penalty=0.0,
-        presence_penalty=0.0,
-        stop=["#", '"""'],
-    )
-    text = response["choices"][0]["text"]
-    text = textwrap.indent(f'"""\n{text}\n"""', "    ")
-    print(text)
-    return text
-
-
-def write_test(code):
-
-    parsed = parse_function(code)
-    prompt = f'Please write a test from the following function using pytest: ```{parsed.name}``` with the following signature {parsed.args}, and the following source code {code}, do not return the full function, only return the docstring surrounded by `"""`, indent the docstring to match the function indentation level'
-
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        temperature=0.7,
-        max_tokens=500,
-    )
-    return response["choices"][0]["text"]
-
-
-def refactor_code(code, prompt):
-
-    prompt = f"refactor the following code to {prompt}\n\n{code}"
-
-    print(f"generating a response for \n\n{prompt}")
-    print("-" * 80)
-    print()
-
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        temperature=0.7,
-        max_tokens=1500,
-    )
-    text = response["choices"][0]["text"]
-    print(text)
-
-    return text
-
-
-def write_blog(code):
-    """
-
-    Write a blog post about the given code.
-
-    This function takes in a code snippet as an argument and generates a blog post about it using OpenAI's Completion API.
-
-    Args:
-        code (str): The code snippet to generate a blog post about.
-
-    Returns:
-        str: A blog post about the given code snippet.
-
-    Example:
-        response = write_blog("def hello_world():\n    print('Hello World!')")
-        print(response)
-
-    """
-
-    prompt = f"Write a blog post about the following code in Markdown. \n\n```{code}```"
-
-    print(f"generating a response for {prompt[:200]}")
-
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        temperature=0.7,
-        max_tokens=500,
-    )
-    return response["choices"][0]["text"]
-
-
-Function = namedtuple("Function", "name, args")
-
-
-def parse_function(code: str) -> Function:
-    tree = ast.parse(code)
-    args = []
-    name = None
-
-    for node in ast.walk(tree):
-        if isinstance(node, ast.FunctionDef) or isinstance(node, ast.AsyncFunctionDef):
-            name = node.name
-            args = [
-                f"{arg.arg}:"  # {arg.annotation.value}"
-                if arg.annotation
-                else f"{arg.arg}: None"
-                for arg in node.args.args
-            ]
-    if name is None:
-        raise ValueError("input is not a FunctionDef")
-    return Function(name, args)
-
-
-# code_str = """def example_function(arg1:int,arg2:str)->int:
-#     pass"""
-# name, args, return_annotation = extract_function_signature(code_str)
-# print(name)
-# print(args)
-# print(return_annotation)
-
-
-def parse_code():
-    tree = ast.parse(my_code)
-    args = []
-
-    for node in ast.walk(tree):
-        if isinstance(node, ast.FunctionDef):
-            name = node.name
-            args = [
-                f"{arg.arg}: {arg.annotation.value}"
-                if arg.annotation
-                else f"{arg.arg}: None"
-                for arg in node.args.args
-            ]
-    return name, args
