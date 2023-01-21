c974e5f92a88a845423fead937f415f1ccfeae10
Waylon S. Walker <waylon@waylonwalker.com>
2023-01-21 17:22:38 -0600

    test that load_prompt works without a profile

--- tests/test_load_prompt.py
+++ tests/test_load_prompt.py
@@ -9,6 +9,7 @@ def config():
         "prompts": {
             "prompt1": {"profile": "profile1", "key1": "value1"},
             "prompt2": {"profile": "profile2", "key2": "value2"},
+            "prompt3": {"key1": "value1"},
         },
         "profiles": {"profile1": {"key3": "value3"}, "profile2": {"key4": "value4"}},
     }
@@ -22,3 +23,6 @@ def test_load_prompt(config, mocker):
 
     prompt2_config = load_prompt("prompt2")
     assert prompt2_config == {"key2": "value2", "key4": "value4"}
+
+    prompt3_config = load_prompt("prompt3")
+    assert prompt3_config == {"key1": "value1"}
