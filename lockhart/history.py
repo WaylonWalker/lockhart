41718a35f5bbd36cd691c59b3dd2c5cd279e7fe1
Waylon S. Walker <waylon@waylonwalker.com>
2023-01-22 11:35:33 -0600

    use Pathlib

--- lockhart/history.py
+++ lockhart/history.py
@@ -1,36 +1,28 @@
 #!/usr/bin/env python
 
-import os
+from pathlib import Path
 import pickle
 
 from lockhart.console import console
 
 
 def save_history(history):
-    """
-    Save the history to a file.
-    """
+    """Save the history to a file."""
     console.log(f"saving\n{history}")
-    home = os.path.expanduser("~")
-    config_dir = os.path.join(home, ".config", "lockhart")
-    if not os.path.exists(config_dir):
-        console.log(f"creating {config_dir}")
-        os.makedirs(config_dir)
-    history_file = os.path.join(config_dir, "history.pkl")
-    with open(history_file, "wb") as f:
+    config_dir = Path.home() / ".config" / "lockhart"
+    config_dir.mkdir(parents=True, exist_ok=True)
+    history_file = config_dir / "history.pkl"
+    with history_file.open("wb") as f:
         pickle.dump(history, f)
     console.log(f"wrote history")
 
 
 def load_history():
-    """
-    Load the history from a file.
-    """
-    home = os.path.expanduser("~")
-    config_dir = os.path.join(home, ".config", "lockhart")
-    history_file = os.path.join(config_dir, "history.pkl")
-    if os.path.exists(history_file):
-        with open(history_file, "rb") as f:
+    """Load the history from a file."""
+    config_dir = Path.home() / ".config" / "lockhart"
+    history_file = config_dir / "history.pkl"
+    if history_file.exists():
+        with history_file.open("rb") as f:
             history = pickle.load(f)
             console.log(f"loaded {len(history)} items into history")
             return history
