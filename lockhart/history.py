abf7d5900fc89a2c5f1890ebf442411f1a3ff34d
Waylon S. Walker <waylon@waylonwalker.com>
2023-01-22 13:14:25 -0600

    make history_file configurable

--- lockhart/history.py
+++ lockhart/history.py
@@ -6,21 +6,20 @@ import pickle
 from lockhart.console import console
 
 
-def save_history(history):
+def save_history(history, history_file=None):
     """Save the history to a file."""
-    console.log(f"saving\n{history}")
-    config_dir = Path.home() / ".config" / "lockhart"
-    config_dir.mkdir(parents=True, exist_ok=True)
-    history_file = config_dir / "history.pkl"
+    if history_file is None:
+        history_file = get_history_file()
+    console.log(f"saving\n{history} to {history_file}")
     with history_file.open("wb") as f:
         pickle.dump(history, f)
     console.log(f"wrote history")
 
 
-def load_history():
+def load_history(history_file=None):
     """Load the history from a file."""
-    config_dir = Path.home() / ".config" / "lockhart"
-    history_file = config_dir / "history.pkl"
+    if history_file is None:
+        history_file = get_history_file()
     if history_file.exists():
         with history_file.open("rb") as f:
             history = pickle.load(f)
@@ -29,6 +28,13 @@ def load_history():
     else:
         console.log(f"history_file does not exist, returning empty history")
         return []
+
+
+def get_history_file():
+    config_dir = Path.home() / ".config" / "lockhart"
+    config_dir.mkdir(parents=True, exist_ok=True)
+    history_file = config_dir / "history.pkl"
+    return history_file
 
 
 if __name__ == "__main__":
