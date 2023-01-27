114f179ac352808024aa42c0fd1d276bbd9a30ab
Waylon S. Walker <waylon@waylonwalker.com>
2023-01-27 17:54:50 -0600

    craeate HISTORY mock fixture

--- tests/test_tui.py
+++ tests/test_tui.py
@@ -1,12 +1,13 @@
 from lockhart.tui.app import RequestApp
 
 
-async def test_run_app():
+async def test_run_app(load_history):
     async with RequestApp().run_test() as request_app:
         assert request_app.app._running
+        assert load_history.call_count == 1
 
 
-async def test_press_next():
+async def test_press_next(load_history):
     async with RequestApp().run_test() as request_app:
         await request_app.press("j")
         assert request_app.app.i == 1
@@ -14,9 +15,11 @@ async def test_press_next():
         assert request_app.app.i == 2
         await request_app.press("j")
         assert request_app.app.i == 3
+        assert load_history.call_count == 1
+        assert load_history.called_with()
 
 
-async def test_press_prev():
+async def test_press_prev(load_history):
     async with RequestApp().run_test() as request_app:
         await request_app.press("k")
         assert request_app.app.i == len(request_app.app.history) - 1
@@ -24,17 +27,22 @@ async def test_press_prev():
         assert request_app.app.i == len(request_app.app.history) - 2
         await request_app.press("k")
         assert request_app.app.i == len(request_app.app.history) - 3
+        assert load_history.call_count == 1
+        assert load_history.called_with()
 
 
-async def test_press_next_rollover():
+async def test_press_next_rollover(load_history):
     async with RequestApp().run_test() as request_app:
         for _ in range(len(request_app.app.history)):
             await request_app.press("j")
         assert request_app.app.i == 0
+        assert load_history.call_count == 1
+        assert load_history.called_with()
 
 
-async def test_press_prev_rollover():
+async def test_press_prev_rollover(load_history):
     async with RequestApp().run_test() as request_app:
         for _ in range(len(request_app.app.history)):
             await request_app.press("k")
         assert request_app.app.i == 0
+        assert load_history.call_count == 1
